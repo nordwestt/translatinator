4cec1a0c22fc226a8d6dcf58f28f9d5d7fdf14bc
Thomas Nordentoft <thomas@nordentoft.dk>
2025-07-25 16:17:29 +0200

    Update jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,29 @@
-module.exports = {
-  preset: 'ts-jest',
+import type { JestConfigWithTsJest } from 'ts-jest';
+
+const config: JestConfigWithTsJest = {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   roots: ['<rootDir>/src', '<rootDir>/tests'],
   testMatch: ['**/__tests__/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
+      useESM: true,
       tsconfig: {
         types: ['jest', 'node']
       }
     }],
   },
+  extensionsToTreatAsEsm: ['.ts'],
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
-    '!src/cli.ts', // CLI is harder to test
+    '!src/cli.ts',
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'html'],
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
-  testTimeout: 30000, // 30 seconds for API calls
-  maxWorkers: 1, // Run tests sequentially to avoid file system interference
+  testTimeout: 30000,
+  maxWorkers: 1,
 };
+
+export default config;
